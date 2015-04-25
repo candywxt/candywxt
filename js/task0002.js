132167cd341ca61e61b3016a48be28cad707397c
candywxt <871547752@qq.com>
2015-04-25 23:20:16 +0800

    add isSiblingNode

--- js/task0002.js
+++ js/task0002.js
@@ -1,75 +1,44 @@
 var hd = document.getElementById("a");
-var b = document.getElementById("b");
-var newclass = hd.className
+console.log(b.parentNode);
+var newclass = hd.className;
 function addClass(element,newClassName) {
     //your implement
     element.className = newClassName;
     
 }
-addClass(hd,"newclass");
+//addClass(hd,"newclass");
 
 //移除dom中样式oldClassName
 function removeClass(element,oldClassName) {
     //your implement
-    var classLis = element.classList;
-    var newlis = [];
-    for(var i = 0 , l = classLis.length; i <= l;i++){
-        if(classLis[i] != oldClassName){
-           newlis.push(classLis[i]);
-           console.log(newlis);
-        }
-    }
-    element.classList = '';
-    console.log(element.classList);
-    element.classList = newlis;
-    console.log(element.classList);
-}
+	if(!element) return;
+	var elementClassName = element.className;
+	if(elementClassName.length == 0) return;
+	if(elementClassName == oldClassName){
+		//element.className = ""；
+		return;
+	}
+	if(elementClassName.match(new RegExp("(^|\\s)"+oldClassName+"(\\s|$)")))
+		element.className = elementClassName.replace((new RegExp("(^|\\s)" + oldClassName + "(\\s|$)")), " ");
+};
 removeClass(hd,"bg")
 console.log(hd.classList);
 
 //判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
 function isSiblingNode(element,siblingNode){
     //your implement
-    var oParent = element.parent;
-    console.log(oParent);
+    if(element.parentNode == siblingNode.parentNode) return true;
 }
 console.log(isSiblingNode(hd,b));
 
 //获取dom相对于浏览器的窗口的位置，返回一个对象{x，y}
 function getPosition(element) {
     //your implement
-    var posX = element.clientLeft,
-        posY = element.clientTop,
+    var posX = this.offsetLeft,
+        posY = this.offsetTop,
         result = new Object;
     result.x = posX;
     result.y = posY;
     return result;
 }
 console.log(getPosition(b));
-Function.prototype.time = function() {
-    var t1 = +new Date()
-    ,   foo = this()
-    ,   t2 = +new Date();
-    return t2-t1;
-}
-Function.prototype.bind = function(ob){
-    var fn = this
-    , slice = Array.prototype.slice
-    , args = slice.call(arguments,l)
-    return function(){
-        return fn.apply(ob,args.concat(slice.apply(arguments)))
-    }
-}
-Array.prototype.delRepeat = function(){
-    var newArray = new Array();
-    var len = this.length;
-    for(var i = 0;i<len;i++){
-        for(var j=i+1;j<len;j++){
-            if(this[i]===this[j]){
-                j=++i;
-            }
-        }
-        newArray.push(this[i]);
-    }
-    return newArray;
-}
